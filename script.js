2073a04534fa75eae28380f27fff23cf39de27b3
jeetporia <poriajeet@gmail.com>
2021-07-17 23:56:32 +0530

    optimized code

--- script.js
+++ script.js
@@ -7,6 +7,13 @@ const image2 = document.getElementById('image2');
 const image3 = document.getElementById('image3');
 const textBox = document.getElementById('text-box');
 
+// for light or dark images
+function imageMode(color) {
+    image1.src = `img/undraw_conceptual_idea_${color}.svg`;
+    image2.src = `img/undraw_feeling_proud_${color}.svg`;
+    image3.src = `img/undraw_proud_coder_${color}.svg`;
+}
+
 //Dark Mode Style
 function darkMode() {
     nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
@@ -14,13 +21,16 @@ function darkMode() {
     console.log(toggleIcon);
 
     toggleIcon.children[0].textContent = 'Dark Mode';
-    toggleIcon.children[1].classList.remove('fa-sun');
-    toggleIcon.children[1].classList.add('fa-moon');
+    // instead of adding and removing the class use replace function
+    // toggleIcon.children[1].classList.remove('fa-sun');
+    // toggleIcon.children[1].classList.add('fa-moon');
+    toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
 
-    // for images
-    image1.src = 'img/undraw_conceptual_idea_dark.svg';
-    image2.src = 'img/undraw_feeling_proud_dark.svg';
-    image3.src = 'img/undraw_proud_coder_dark.svg';
+    // for images - instead of this repeating code we can seperate this 
+    // image1.src = 'img/undraw_conceptual_idea_dark.svg';
+    // image2.src = 'img/undraw_feeling_proud_dark.svg';
+    // image3.src = 'img/undraw_proud_coder_dark.svg';
+    imageMode('dark')
 }
 
 function lightMode() {
@@ -28,12 +38,15 @@ function lightMode() {
     textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
 
     toggleIcon.children[0].textContent = 'Light Mode';
-    toggleIcon.children[1].classList.remove('fa-moon');
-    toggleIcon.children[1].classList.add('fa-sun');
+    // instead of add and remove use replace function to do this
+    // toggleIcon.children[1].classList.remove('fa-moon');
+    // toggleIcon.children[1].classList.add('fa-sun');
+    toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun')
 
-    image1.src = 'img/undraw_conceptual_idea_light.svg';
-    image2.src = 'img/undraw_feeling_proud_light.svg';
-    image3.src = 'img/undraw_proud_coder_light.svg';
+    imageMode('light');
+    // image1.src = 'img/undraw_conceptual_idea_light.svg';
+    // image2.src = 'img/undraw_feeling_proud_light.svg';
+    // image3.src = 'img/undraw_proud_coder_light.svg';
 }
 
 //  switch theme dynamically
